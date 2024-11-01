006c54d7acf78abe7e22a26f35e781c7c62ba1b4
CryptoMan0x <156699519+CryptoMan0x@users.noreply.github.com>
2024-11-01 14:51:23 -0400

    more updates

--- data/mainnet.ts
+++ data/mainnet.ts
@@ -1,12 +1,10 @@
 import type { Paymaster, Token } from "~/types";
 
-// TO-DO: Update token info and paymaster address with mainnet data
-
 const CUSTOM_USDC_TOKEN: Token = {
-  address: "0x27553b610304b6AB77855a963f8208443D773E60",
-  l1Address: "0xBF4FdF7BF4014EA78C0A07259FBc4315Cb10d94E",
-  symbol: "CustomUSDC",
-  name: "Custom USD coin",
+  address: "0x6386dA73545ae4E2B2E0393688fA8B65Bb9a7169",
+  l1Address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  symbol: "USDC",
+  name: "USDC",
   decimals: 6,
   iconUrl: "/img/usdc.svg",
   l1BridgeAddress: "0x3f842b5FaD08Bac49D0517C975d393f5f466Fd3b",
--- data/testnet.ts
+++ data/testnet.ts
@@ -3,8 +3,8 @@ import type { Paymaster, Token } from "~/types";
 const CUSTOM_USDC_TOKEN: Token = {
   address: "0x27553b610304b6AB77855a963f8208443D773E60",
   l1Address: "0xBF4FdF7BF4014EA78C0A07259FBc4315Cb10d94E",
-  symbol: "CustomUSDC",
-  name: "Custom USD coin",
+  symbol: "MockUSDC",
+  name: "Mock USD coin",
   decimals: 6,
   iconUrl: "/img/usdc.svg",
   l1BridgeAddress: "0x3f842b5FaD08Bac49D0517C975d393f5f466Fd3b",
